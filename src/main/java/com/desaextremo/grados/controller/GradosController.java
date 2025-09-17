3e8bbb87a9d58c18b051c18999b800054ecb4aa0
desaextremo <desarrolloextremo@gmail.com>
2025-09-17 17:55:35 -0500

    chore: Se elimina metodo de mensajes

--- src/main/java/com/desaextremo/grados/controller/GradosController.java
+++ src/main/java/com/desaextremo/grados/controller/GradosController.java
@@ -30,9 +30,4 @@ public class GradosController {
     public double kelvinACentigrados(@PathVariable("grades") double k) {
         return k - 273.15;
     }
-
-    @GetMapping("/ping")
-        public String ping() {
-        return "Mensaje de respuesta";
-    }
 }
