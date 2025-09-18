e61b92332f265e7bd694c0b42ed37622ec36ec98
desaextremo <desarrolloextremo@gmail.com>
2025-09-17 19:56:09 -0500

    chore: Se agrega metodo de mensajes

--- src/main/java/com/desaextremo/grados/controller/GradosController.java
+++ src/main/java/com/desaextremo/grados/controller/GradosController.java
@@ -11,26 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class GradosController {
 
-    @GetMapping("/centtofahr/{grades}")
-    public double centigradosAFahrenheit(@PathVariable("grades") double c) {
-        return (c * 9.0 / 5.0) + 32.0;
-    }
-
-    @GetMapping("/fahrtocent/{grades}")
-    public double fahrenheitACentigrados(@PathVariable("grades") double f) {
-        return (f - 32.0) * 5.0 / 9.0;
-    }
-
-    @GetMapping("/centtokelv/{grades}")
-    public double centigradosAKelvin(@PathVariable("grades") double c) {
-        return c + 273.15;
-    }
-
-    @GetMapping("/kelvintocent/{grades}")
-    public double kelvinACentigrados(@PathVariable("grades") double k) {
-        return k - 273.15;
-    }
-
     @GetMapping("/ping")
         public String ping() {
         return "Mensaje de respuesta";
